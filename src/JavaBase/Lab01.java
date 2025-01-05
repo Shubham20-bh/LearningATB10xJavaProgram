c84f1c266c0f36ee7c0b0b7ce77520f085f4b074
Shubham.2.B <shubhambhisikar20@gmail.com>
2025-01-05 07:41:22 +0100

    Lab01

---
+++ src/JavaBase/Lab01.java
@@ -0,0 +1,18 @@
+package JavaBase;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        int a =40;
+        int b=20;
+        int c=80;
+        System.out.println("Welcome to IntellJ");
+        System.out.println("Add : "+ 2+3);
+        System.out.println("Sub : "+ (a-b));
+        System.out.println("multi : "+ a*b );
+        System.out.println("division" + a/b);
+        System.out.println("mod/quotient even number: "+(a%b == 0));
+
+
+    }
+
+}
