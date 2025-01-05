7edade35a6858ccd9b803cd8fc3599b29d9153b0
Shubham.2.B <shubhambhisikar20@gmail.com>
2025-01-05 15:23:57 +0100

    Lab03

---
+++ src/JavaProgram/Lab03.java
@@ -0,0 +1,19 @@
+package JavaProgram;
+
+public class Lab03 {
+    public static void main(String[] args) {
+        Lab03 obj = new Lab03();
+        obj.SwapTwoString();
+    }
+    public  void SwapTwoString() {
+        String S1= "Java";
+        String S2= "Selenium";
+        String temp=S2; //Java
+
+        S2=S1;
+        S1=temp;
+
+        System.out.println("S1 after swaping "+S1 + " S2 after swaping "+S2);
+
+    }
+}
