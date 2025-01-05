1405ce5ad55b681daf0c88f16d01f8c3fb3cd441
Shubham.2.B <shubhambhisikar20@gmail.com>
2025-01-05 13:49:48 +0100

    Lab2

---
+++ src/JavaProgram/Lab02.java
@@ -0,0 +1,13 @@
+package JavaProgram;
+
+public class Lab02 {
+    public static void main(String[] args) {
+        int m=10;
+        int n=5;
+        //swap 2 numbers
+        int temp =m;
+        m=n;
+        n = temp;
+        System.out.println("m :"+m + " n :"+n);
+    }
+}
